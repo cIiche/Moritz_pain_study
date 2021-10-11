682d56bdb34a4aa29d5e3991ee23e4d2489faa59
Henry <Henry@DESKTOP-J94A4L0>
2021-10-11 02:04:50 -0700

    got 8.18 coherence plotter working, plot needs labeling and work

--- calc_coherence.m
+++ calc_coherence.m
@@ -1,6 +1,9 @@
 %% Authors: Kat Floerchinger, Hannah Mach, Henry Tan
 %this code is meant to set the index_stim equal to 60 so that we can
 %standardize the length to calculate reliable p values 
+fs=tickrate(1);
+time1=1:dataend(1);
+time=time1/fs/60;
 
 %Organize data into structure array
 alldata=[]; 
@@ -17,16 +20,25 @@ end
 names={'RILdata','LBLAdata','RBLAdata','LILdata', 'stimdata'};
 foranalysis={'RILdata','LBLAdata','RBLAdata','LILdata'}; 
 
+% %% plot raw data
+% figure
+% for i=1:length(names)-1
+%     subplot(length(names),1,i)
+%     plot(time,alldata.(char(names(i)))) % this is plotting time in minutes, but if you want seconds, use timesec instead of time
+%     title(names(i));
+% end
+% xlabel('time (minutes)')
+
 %% Filter the raw data with a lowpass butterworth filter and condition removing points > 4std from data 
 %Note: it's better to filter the raw data before STAs because this limits
 %the end effects that are inevitably created by the filter (you'd get end
 %effects at the ends of each STA, vs end effects only at the beginning and
 %end of the time series data
 
-% lowEnd = 1; % Hz
-% highEnd = 50; % Hz
-lowEnd = 5; % Hz
-highEnd = 8; % Hz
+% In rodents, fear regula86 tion is associated with theta band (5-8 Hz) LFP synchrony 
+% between the infralimbic cortex (IL, homologue of human PFC) and basolateral amygdala (BLA)
+lowEnd = 3; % Hz
+highEnd = 100; % Hz
 filterOrder = 3; % Filter order (e.g., 2 for a second-order Butterworth filter). Try other values too
 [b, a] = butter(filterOrder, [lowEnd highEnd]/(fs/2)); % Generate filter coefficients
 
@@ -35,13 +47,14 @@ filteredData.(char(names(ii))) = filtfilt(b, a,alldata.(char(names(ii)))); % App
 % is this correct here? before STAs? - in vis stim, for stats_analysis is
 % filtered like this, after STA creation 
 
-% for some reason, std and mean create NaN. This is commented out for now
+% for some reason, std and mean create NaN when filtering 5-8Hz. Works for
+% 3-100
 deviation=std(filteredData.(char(names(ii))), 'omitnan');
 trialmean = mean(filteredData.(char(names(ii))), 'omitnan');
 filteredData.(char(names(ii))) = filteredData.(char(names(ii)))(filteredData.(char(names(ii)))<trialmean+4*deviation);
 end
 
-% name pages of wavs
+%% 
 % to match sizes 
 if length(filteredData.RILdata) < length(filteredData.RBLAdata) 
     b = length(filteredData.RILdata);
@@ -49,16 +62,23 @@ else
     b = length(filteredData.RBLAdata);
 end
 
-% is this too big to plot? 
 % figure 
 % plot(filteredData.RILdata)
 % hold on 
 % plot(filteredData.RBLAdata)
 
-wav1 = filteredData.RILdata(1:1000);
-wav2 = filteredData.RBLAdata(1:1000); 
+wav1 = filteredData.RILdata(1:b);
+wav2 = filteredData.RBLAdata(1:b); 
 cxy = mscohere(wav1, wav2) ;
+% figure
+% plot(cxy)
+% title('Magnitude-Squared Coherence')
 
+% should we use mean or median?
+cohmed = median(cxy');
+cohmean = mean(cxy') ; 
+
+% % name pages of wavs
 %   wav(:,:,1) = filteredData.RILdata(1:b);
 %   wav(:,:,2) = filteredData.RBLAdata(1:b);
 %   

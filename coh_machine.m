d90ed91b5dca855c2fcc69ec6ad112572865b754
Administrator <Administrator@128.95.117.185>
2021-11-15 16:19:08 -0800

    added coh plotter

---
+++ coh_machine.m
@@ -0,0 +1,165 @@
+clear all
+clc
+% https://www.fieldtriptoolbox.org/faq/how_can_i_import_my_own_dataformat/ 
+
+% this script uses the fieldtrip toolbox to calculate coherence 
+% for the Moritz Lab Pain Study 
+
+% finding fieldtrip toolbox ; It is most convenient to have the addpath and ft_defaults in a script with the name startup.m, which is located in your own MATLAB directory. See this information from MathWorks.
+restoredefaultpath
+addpath 'C:\Users\Administrator\AppData\Roaming\MathWorks\MATLAB Add-Ons\Collections\FieldTrip'
+ft_defaults
+
+% Preprocessing options that you should only use when you are calling FT_PREPROCESSING with
+% also the second input argument "data" are
+%   cfg.trials        = 'all' or a selection given as a 1xN vector (default = 'all')
+
+% filePaths = {'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_18_2021 rat\8.18.21 5 min\', 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_24_2021 rat 1\8.24.21 r1 5 min', 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_24_2021 rat 2\8.24.21 r2 5 min'};
+% fileNames = {'after stim 0', 'after stim 1', 'after stim 2', 'after stim 3', 'after stim 4', 'after stim 5', 'after stim 6', 'after stim 7', 'after stim 8', 'after stim 9'};
+% cohmat = zeros(3,10);  
+% 
+% whichrat = input("What rat would you like to run? ('1' = 8/18 r1, '2' = 8/24 r2, '3' = 8/24 r3): ") ; 
+% filePath = filePaths{whichrat} ;
+% % for 8/18 
+% set_channels=[1 2 3 4 5 6 7] ;
+% RIL=set_channels(2);LBLA=set_channels(4);RBLA=set_channels(1);LIL=set_channels(3);stim=set_channels(5) ;
+% fs = 20000 ;
+% for i = 1:3
+%     fileName = fileNames{i} ;
+    
+%% opening file 
+filePath = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_18_2021 rat\8.18.21 5 min\';
+% filePath = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_24_2021 rat 1\8.24.21 r1 5 min\';
+% filePath = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_24_2021 rat 2\8.24.21 r2 5 min\'; 
+fileName= 'last 5 min of baseline' ;
+load([filePath, fileName]);
+
+set_channels=[1 2 3 4 5 6 7] ;
+% % 8/18  
+RIL=set_channels(2);LBLA=set_channels(4);RBLA=set_channels(1);LIL=set_channels(3);stim=set_channels(5) ;
+% % 8/24 r1 
+% RIL=set_channels(4);LBLA=set_channels(1);RBLA=set_channels(2);LIL=set_channels(3);stim=set_channels(5) ;
+% 8/24 r2
+% RIL=set_channels(2);LBLA=set_channels(1);RBLA=set_channels(4);LIL=set_channels(3);stim=set_channels(5) ;
+fs = 20000 ;
+
+timeax=1:dataend(1); %set time axis
+time=timeax/fs/60;%frames to seconds to minutes (these are the time values for each data point)
+timesec=timeax./fs;
+tottime=length(timeax)./fs./60; % total experiment block length in minutes 
+%% Organize data into structure array
+
+alldata=[]; %initialize structure array (alldata is a struct)
+
+alldata.RILdata=data(datastart(RIL):dataend(RIL)); % Call different fields as StructName.fieldName-> Struct is alldata and field is S1dataR
+alldata.LBLAdata=data(datastart(LBLA):dataend(LBLA));
+alldata.RBLAdata=data(datastart(RBLA):dataend(RBLA));
+alldata.LILdata=data(datastart(LIL):dataend(LIL));
+alldata.stimdata=data(datastart(stim):dataend(stim));
+
+%% making data structure fieldtrip compatible 
+% data.label      % cell-array containing strings, Nchan*1
+% data.fsample    % sampling frequency in Hz, single number
+% data.trial      % cell-array containing a data matrix for each
+%                 % trial (1*Ntrial), each data matrix is a Nchan*Nsamples matrix
+% data.time       % cell-array containing a time axis for each
+%                 % trial (1*Ntrial), each time axis is a 1*Nsamples vector
+% data.trialinfo  % this field is optional, but can be used to store
+%                 % trial-specific information, such as condition numbers,
+%                 % reaction times, correct responses etc. The dimensionality
+%                 % is Ntrial*M, where M is an arbitrary number of columns.
+% data.sampleinfo % optional array (Ntrial*2) containing the start and end
+%                 % sample of each trial
+eegdata=[];
+eegdata.label = {'RBLA' ; 'RIL'; 'LIL'; 'LBLA'} ; 
+eegdata.fsample = 20000;
+% for 5 min chopped 
+eegdatamat = [alldata.RBLAdata; alldata.RILdata; alldata.LILdata; alldata.LBLAdata];
+eegdata.trial = {eegdatamat} ;
+% for whole experiment
+% fullsamp = length(RBLAdata) ;
+% trialsamp = 
+% data.trial
+eegdata.time = {time}; % in minutes 
+eegdata.sampleinfo = [1 length(alldata.RBLAdata)] ; 
+
+%% bandpass automation 
+% 
+% lowend = input('What lowend frequency for coherence analysis?: ') ; 
+% highend = input('What highend frequency for coherence analysis?: ') ;
+% disp('Downsampling rate = highend x 2 
+
+%% downsampling 
+% Use as
+%   [data] = ft_resampledata(cfg, data)
+%
+% The data should be organised in a structure as obtained from the FT_PREPROCESSING
+% function. The configuration should contain
+%   cfg.resamplefs      = frequency at which the data will be resampled (default = 256 Hz)
+%   cfg.detrend         = 'no' or 'yes', detrend the data prior to resampling (no default specified, see below)
+%   cfg.demean          = 'no' or 'yes', whether to apply baseline correction (default = 'no')
+%   cfg.baselinewindow  = [begin end] in seconds, the default is the complete trial (default = 'all')
+%   cfg.feedback        = 'no', 'text', 'textbar', 'gui' (default = 'text')
+%   cfg.trials          = 'all' or a selection given as a 1xN vector (default = 'all')
+%   cfg.sampleindex     = 'no' or 'yes', add a channel with the original sample indices (default = 'no')
+cfg.resamplefs = 108 ;
+% cfg.resamplefs = 30 ;
+cfg.detrend = 'yes' ;
+cfg.demean = 'no' ;
+cfg.feedback = 'text' ;
+cfg.trials = 'all';
+cfg.sampleindex = 'no' ;
+data = ft_resampledata(cfg, eegdata) ;
+data.sampleinfo = [1 length(data.time{1})] ; 
+
+%% preproccesing  
+
+% cfg = [];
+% % bandpass filter 
+% cfg.bpfilter = 'yes';
+% % bandpass frequency range [lowFreq highFreq] in Hz
+% cfg.bpfreq = [5 8]; 
+% % cfg.datafile = 'after stim 0.mat' ;
+% % cfg.headerfile = 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_18_2021 rat\8.18.21 5 min\';
+% % cfg.dataset = 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_18_2021 rat\8.18.21 5 min\after stim 0.mat\' ;
+% % cfg.dataset = alldata ;
+% cfg.dataset = eegdata ;
+% data_eeg = ft_preprocessing(cfg);
+
+%% ft_freqanalysis for the computation of the cross- and power spectra
+
+cfg            = [];
+cfg.output     = 'powandcsd';
+cfg.method     = 'mtmfft';
+%   cfg.foilim     = [begin end], frequency band of interest
+cfg.foilim     = [5 56];
+cfg.tapsmofrq  = 5;
+cfg.keeptrials = 'no';
+% cfg.channel    = {'RBLA' 'RIL'};
+cfg.channelcmb = {'RBLA' 'RIL'};
+cfg.pad        = 'nextpow2';
+freq           = ft_freqanalysis(cfg, data);
+
+cfg            = [];
+cfg.method     = 'coh';
+cfg.channelcmb = {'RBLA' 'RIL'};
+fd             = ft_connectivityanalysis(cfg, freq);
+
+% cohmat(whichrat, i) = median(fd.cohspctrm) ;
+% %% plotting coherence with ft_singleplotER 
+
+cfg                  = [];
+cfg.parameter        = 'cohspctrm';
+cfg.xlim             = 'maxmin';
+cfg.refchannel       = 'RIL';
+cfg.showlabels       = 'yes';
+% figure; ft_multiplotER(cfg, fd)
+% cfg.channel = 'MRC21';
+figure; 
+ft_singleplotER(cfg, fd);
+ylabel('Coherence')
+xlabel('Frequency (Hz)')
+title('Connectivity between RIL & RBLA with reference to RIL')
+a = median(fd.cohspctrm);
+a
+% end 
--- coherence_plotter.m
+++ coherence_plotter.m
@@ -1,103 +1,138 @@
-%%  Authors: Kat Floerchinger, Hannah Mach, Henry Tan
-%This code plots dcoherence/dt for the Moritz Rat Pain Study 
-clear all
-close all
-clc
-%% reading rat experiment files 
-
-% whichrat = input("Which experiment to run? '1' = 8/18/21 rat, '2' = 8/24/21 rat 1, '3' = 8/24/21 rat 2: "); 
+% %%  Authors: Kat Floerchinger, Hannah Mach, Henry Tan
+% %This code plots dcoherence/dt for the Moritz Rat Pain Study 
+% clear all
+% close all
+% clc
+% %% reading rat experiment files 
 % 
-% if whichrat == 1 
-%     MainDirectory = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_18_2021 rat\';
-%     file1={'last 5 min baseline\', '5 min stim\', 'after stim 1\','after stim 2\','after stim 3\', 'after stim 4\', 'after stim 5\', 'after stim 6\', 'after stim 7\', 'after stim 8\', 'after stim 9\', 'after stim 10\','after stim 11\','after stim 12\'};
-%     file1={'last 5 min baseline\', '5 min stim\', 'after stim 1\','after stim 2\','after stim 3\', 'after stim 4\', 'after stim 5\', 'after stim 6\', 'after stim 7\', 'after stim 8\', 'after stim 9\'};
-% elseif whichrat == 2 
-%     MainDirectory = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_24_2021 rat 1\';
-%     file1={'last 5 min baseline\', '5 min stim\', 'after stim 1\','after stim 2\','after stim 3\', 'after stim 4\', 'after stim 5\', 'after stim 6\', 'after stim 7\', 'after stim 8\', 'after stim 9\'};
-% elseif whichrat == 3 
-%     MainDirectory = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_24_2021 rat 2\';
-%     file1={'last 5 min baseline\', '5 min stim\', 'after stim 1\','after stim 2\','after stim 3\', 'after stim 4\', 'after stim 5\', 'after stim 6\', 'after stim 7\', 'after stim 8\', 'after stim 9\'};
-% end 
-
-
-%% creating coherence Matrix to store values 
-
-%% Reading experiment dates 
-
-
-file1= {'8.18.21 5 min\', '8.24.21 r1 5 min\', '8.24.21 r2 5 min\'} ;
-str=string(file1);
-Directories = {'C:\Users\Administrator\MATLAB\Projects\Pain_Study\Data\08_18_2021 rat\', 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_24_2021 rat 1\', 'C:\Users\Administrator\MATLAB\Projects\Pain_Study\Data\08_24_2021 rat 2\'} ; 
-dirs = string(Directories) ; 
-% MainDirectory = {'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\'} ; 
-
-% for i = 1:length(dirs) 
-% for rat = 1:length(dirs)
-coh_matrix = zeros(3,14) ;
-for f= 1:length(str) 
-% for f=1
-MainDirectory = dirs{f} ;
-folder = fullfile(MainDirectory,str{f});
-
-%Change what is in the string depending on which file\files you want to run
-file_list = dir([folder 'after stim *.mat']);
-% baseline = dir([folder 'last 5 min of baseline.mat']); 
-
-set_channels=[1 2 3 4 5]; 
-ch_names={'RIL','LBLA','RBLA', 'LIL', 'stimdata'}; 
-trial_names={' FIRST LIGHT ONLY' 'LIGHT + US' ' SECOND LIGHT ONLY'};
-
-%% channel configuration
-
-% if folder == 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_18_2021 rat\8.18.21 5 min\' 
-if f == 1 
-% if i == 1 
-    RIL=set_channels(2);LBLA=set_channels(4);RBLA=set_channels(1);LIL=set_channels(3);stim=set_channels(5) ;
-    
-elseif f == 2
-    
-% end 
-% if folder == 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_24_2021 rat 1\8.24.21 r1 5 min\'  
-    RIL=set_channels(4);LBLA=set_channels(1);RBLA=set_channels(2);LIL=set_channels(3);stim=set_channels(5) ;
-else 
-% if folder == 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_24_2021 rat 2\8.24.21 r2 5 min\'  
-    RIL=set_channels(2);LBLA=set_channels(1);RBLA=set_channels(4);LIL=set_channels(3);stim=set_channels(5) ;
-end 
-
-%% calculate pre-stim RMS for normalization 
-
-% baseline_rms=[];
-% disp(baseline.name);
-% load([folder baseline.name])
-% calc_baseline;
-
-% create matrix to hold data for statistical testing
-for_stats_new = [];
-for_stats_analysis=[];
-%insert thresholding here
-
+% % whichrat = input("Which experiment to run? '1' = 8/18/21 rat, '2' = 8/24/21 rat 1, '3' = 8/24/21 rat 2: "); 
+% % 
+% % if whichrat == 1 
+% %     MainDirectory = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_18_2021 rat\';
+% %     file1={'last 5 min baseline\', '5 min stim\', 'after stim 1\','after stim 2\','after stim 3\', 'after stim 4\', 'after stim 5\', 'after stim 6\', 'after stim 7\', 'after stim 8\', 'after stim 9\', 'after stim 10\','after stim 11\','after stim 12\'};
+% %     file1={'last 5 min baseline\', '5 min stim\', 'after stim 1\','after stim 2\','after stim 3\', 'after stim 4\', 'after stim 5\', 'after stim 6\', 'after stim 7\', 'after stim 8\', 'after stim 9\'};
+% % elseif whichrat == 2 
+% %     MainDirectory = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_24_2021 rat 1\';
+% %     file1={'last 5 min baseline\', '5 min stim\', 'after stim 1\','after stim 2\','after stim 3\', 'after stim 4\', 'after stim 5\', 'after stim 6\', 'after stim 7\', 'after stim 8\', 'after stim 9\'};
+% % elseif whichrat == 3 
+% %     MainDirectory = 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_24_2021 rat 2\';
+% %     file1={'last 5 min baseline\', '5 min stim\', 'after stim 1\','after stim 2\','after stim 3\', 'after stim 4\', 'after stim 5\', 'after stim 6\', 'after stim 7\', 'after stim 8\', 'after stim 9\'};
+% % end 
+% 
+% 
+% %% creating coherence Matrix to store values 
+% 
+% %% Reading experiment dates 
+% 
+% 
+% file1= {'8.18.21 5 min\', '8.24.21 r1 5 min\', '8.24.21 r2 5 min\'} ;
+% str=string(file1);
+% Directories = {'C:\Users\Administrator\MATLAB\Projects\Pain_Study\Data\08_18_2021 rat\', 'C:\Users\Administrator\MATLAB\Projects\Pain_study\Data\08_24_2021 rat 1\', 'C:\Users\Administrator\MATLAB\Projects\Pain_Study\Data\08_24_2021 rat 2\'} ; 
+% dirs = string(Directories) ; 
+% % MainDirectory = {'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\'} ; 
+% 
+% % for i = 1:length(dirs) 
+% % for rat = 1:length(dirs)
 % coh_matrix = zeros(3,14) ;
-
-for z=1:14
-%      if isequal(file_list(z).name,"Trial 2.mat"), continue, end %for 12-23
-    disp(z)% display the number that the code is on in the terminal, do not put a ';' after it 
-    disp(file_list(z).name);% displays the name of the file in the terminal
-    load([folder file_list(z).name]);% bringing the file data into matlab so that the code can run
-    calc_coherence;
-    coh_matrix(f, z) = cohmed ; 
-end
-end 
+% for f= 1:length(str) 
+% % for f=1
+% MainDirectory = dirs{f} ;
+% folder = fullfile(MainDirectory,str{f});
+% 
+% %Change what is in the string depending on which file\files you want to run
+% file_list = dir([folder 'after stim *.mat']);
+% % baseline = dir([folder 'last 5 min of baseline.mat']); 
+% 
+% set_channels=[1 2 3 4 5]; 
+% ch_names={'RIL','LBLA','RBLA', 'LIL', 'stimdata'}; 
+% trial_names={' FIRST LIGHT ONLY' 'LIGHT + US' ' SECOND LIGHT ONLY'};
+% 
+% %% channel configuration
+% 
+% % if folder == 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_18_2021 rat\8.18.21 5 min\' 
+% if f == 1 
+% % if i == 1 
+%     RIL=set_channels(2);LBLA=set_channels(4);RBLA=set_channels(1);LIL=set_channels(3);stim=set_channels(5) ;
+%     
+% elseif f == 2
+%     
+% % end 
+% % if folder == 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_24_2021 rat 1\8.24.21 r1 5 min\'  
+%     RIL=set_channels(4);LBLA=set_channels(1);RBLA=set_channels(2);LIL=set_channels(3);stim=set_channels(5) ;
+% else 
+% % if folder == 'C:\Users\Henry\MATLAB\Projects\Pain_Study\Data\08_24_2021 rat 2\8.24.21 r2 5 min\'  
+%     RIL=set_channels(2);LBLA=set_channels(1);RBLA=set_channels(4);LIL=set_channels(3);stim=set_channels(5) ;
+% end 
+% 
+% %% calculate pre-stim RMS for normalization 
+% 
+% % baseline_rms=[];
+% % disp(baseline.name);
+% % load([folder baseline.name])
+% % calc_baseline;
+% 
+% % create matrix to hold data for statistical testing
+% for_stats_new = [];
+% for_stats_analysis=[];
+% %insert thresholding here
+% 
+% % coh_matrix = zeros(3,14) ;
+% 
+% for z=1:14
+% %      if isequal(file_list(z).name,"Trial 2.mat"), continue, end %for 12-23
+%     disp(z)% display the number that the code is on in the terminal, do not put a ';' after it 
+%     disp(file_list(z).name);% displays the name of the file in the terminal
+%     load([folder file_list(z).name]);% bringing the file data into matlab so that the code can run
+%     calc_coherence;
+%     coh_matrix(f, z) = cohmed ; 
 % end
+% end 
+% % end
+% 
+% 
+% min5 = 1:14 ;
+% plot(min5, coh_matrix)
+% title ('Coherence in 5 Minute Intervals Across 3 Mice')
+% % title ('Coherence in 5 Minute Intervals Across 3 Mice (45 Minutes After Stim)')
+% xlabel ('Time (in 5 Minute Intervals)')
+% ylabel ('Coherence Value')
+% legend ('8/18/21 Rat 1', '8/24/21 Rat 1' , '8/24/21 Rat 2', 'location' , 'best')
+% set(gca,'XTick',(1:14)); %This is going to be the only values affected. 
+% set(gca,'XTickLabel',[-5 0 5 10 15 20 25 30 35 40 45 50 55 60] ); %This is what it's going to appear in those places.
+% 
 
+%% 11/15/21 plotter 
 
-min5 = 1:14 ;
-plot(min5, coh_matrix)
-title ('Coherence in 5 Minute Intervals Across 3 Mice')
-% title ('Coherence in 5 Minute Intervals Across 3 Mice (45 Minutes After Stim)')
-xlabel ('Time (in 5 Minute Intervals)')
-ylabel ('Coherence Value')
-legend ('8/18/21 Rat 1', '8/24/21 Rat 1' , '8/24/21 Rat 2', 'location' , 'best')
-set(gca,'XTick',(1:14)); %This is going to be the only values affected. 
-set(gca,'XTickLabel',[-5 0 5 10 15 20 25 30 35 40 45 50 55 60] ); %This is what it's going to appear in those places.
+figure   
+y1 = [0.8390 0.8618 0.6695 0.5030 0.8514 0.8366 0.8225 0.8483 0.3932 0.8110 0.6881];
+% x1 = [-5 0 5 10 15 20 25 30 35 40 45] ;
+xlabels = {'Baseline', 'during 5 min of US stim', 5, 10, 15, 20, 25, 30, 35, 40, 45} ;
+p1 = plot(y1, 'o-g','DisplayName','rat 1') ;
+hold on 
+y2 = [0.8072 0.8177 0.8431 0.8257 0.9958 0.791 0.7731 0.7740 0.8564 0.7484 0.8049 ] ;
+p2 = plot(y2, 'o-r','DisplayName','rat 2') ;
+hold on 
+y3 = [0.2991 0.2798 0.2890 0.4836 0.3891 0.2541 0.2890 0.2488 0.3916 0.3873 0.4288] ;
+p3 = plot(y3, 'o-b','DisplayName','rat 3') ;
+set(gca,'xtick',[1:11],'xticklabel',xlabels)
+title('RIL & RBLA Coherence (5-8Hz) vs. Time after US Stimulation')
+xlabel('Time after US Stim ends (minutes)') 
+ylabel('Coherence value') 
+legend([p1 p2 p3],{'rat 1','rat 2', 'rat 3'})
 
-
+figure 
+y1 = [0.7099 0.7041 0.6962 0.6479 0.6928 0.7060 0.6576 0.6855 0.6547 0.6558 0.6738];
+% y1 = [0.7072 0.7041 0.6962 0.6479 0.6928 0.7060 0.6576 0.6855 0.6547 0.6558 0.6738];
+x1 = [0 5 10 15 20 25 30 35 40 45] ;
+p1 = plot(x1, y1, 'g','DisplayName','rat 1') ;
+hold on 
+y2 = [0.7780 0.8224 0.7615 0.9971 0.6782 0.6319 0.8619 0.6777 0.6926 0.6052] ;
+p2 = plot(x1, y2, 'r','DisplayName','rat 2') ;
+hold on 
+y3 = [0.5223 0.4932 0.5299 0.6535 0.6994 0.6080 0.5293 0.6023 0.5756 0.7209] ;
+p3 = plot(x1, y3, 'b','DisplayName','rat 3') ;
+set(gca,'xtick',[1:11],'xticklabel',xlabels)
+title('RIL & RBLA Coherence (5-59Hz) vs. Time after US Stimulation')
+xlabel('Time after US Stim ends (minutes)') 
+ylabel('Coherence value') 
+legend([p1 p2 p3],{'rat 1','rat 2', 'rat 3'})
